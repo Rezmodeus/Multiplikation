97a92db0307499d2a9009bac9638b36abc11655e
Rezmodeus <rezmodeus@yahoo.com>
2016-05-11 13:12:12 +0200

    moved stars out of challenges

--- src/App/INITIAL_STATE.js
+++ src/App/INITIAL_STATE.js
@@ -14,7 +14,6 @@ function getStandardChallenges() {
 				level: chNr + 1,
 				id,
 				requiredStars,
-				stars: 0,
 				tables: [tableNr + 1]
 			}
 
@@ -28,68 +27,13 @@ export default immutable.fromJS({
 	nr: 0,
 	gameState: 'start',
 	currentChallenge: null,
+	stats:{},
+	challengeStars:{},
 	modal: {
 		visible: false,
 		type:'',
 		text:''
 	},
 	challenges: getStandardChallenges(),
-	challenges_old: {
-		c1_1: {
-			name: 'Nivå 1',
-			levelType: 'level1',
-			tables: [1],
-			unlocked: true,
-			unlocks: ['c1_2']
-		},
-
-		c1_2: {
-			name: 'Nivå 2',
-			levelType: 'level2',
-			tables: [1],
-			unlocked: true,
-			unlocks: []
-		},
-
-		c1_3: {
-			name: 'Nivå 3',
-			levelType: 'level3',
-			tables: [1],
-			unlocked: true,
-			unlocks: []
-		},
-
-		c1_4: {
-			name: 'Nivå 4',
-			levelType: 'level4',
-			tables: [1],
-			unlocked: true,
-			unlocks: []
-		},
-
-		c1_5: {
-			name: 'Nivå 5',
-			levelType: 'level5',
-			tables: [1],
-			unlocked: true,
-			unlocks: []
-		},
-
-		c2_1: {
-			name: 'Nivå 1',
-			levelType: 'level1',
-			tables: [2],
-			unlocked: false,
-			unlocks: ['c2_2']
-		},
-
-		c2_2: {
-			name: 'Nivå 2',
-			levelType: 'level2',
-			tables: [2],
-			unlocked: false,
-			unlocks: []
-		}
-	},
 	level: {}
 });
