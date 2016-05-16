39c72d27319b1d9fc1126167c30c790d3786ce0b
Rezmodeus <rezmodeus@yahoo.com>
2016-05-17 00:16:48 +0200

    stars counter

--- src/App/components/StartHeader.react.js
+++ src/App/components/StartHeader.react.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import immutable from 'immutable';
 import {Button} from 'react-bootstrap';
+import Star from './Star.react';
 
 export default React.createClass({
 
 	render() {
 		return (
 			<div className="navigation-container">
-				<Button bsStyle="success" bsSize="small" onClick={()=>this.addStars(2)}>add
-					star {this.props.stars}</Button>
+				<h3>{this.props.stars} <Star filled={true}/></h3>
 				<h3>{this.props.currentUser}</h3>
 				<Button className="restart-btn" bsStyle="danger" bsSize="small" onClick={()=>this.props.setModal('NameSelection')}>Byt användare</Button>
 			</div>
